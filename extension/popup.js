953239fb969d1526134358dfe2a2d194014baf80
C.Nelhomme <64921289+D3ller@users.noreply.github.com>
2024-11-18 13:32:34 +0100

    extension

--- extension/popup.js
+++ extension/popup.js
@@ -15,12 +15,52 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
         });
 
         if (response.ok) {
+            appear(document.getElementById("status"));
             document.getElementById("status").innerText = "Login successful!";
+            const profile = await response.json();
+            localStorage.setItem("profile", JSON.stringify(profile.user));
+            console.log(localStorage.getItem("profile"));
+
         } else {
             throw new Error(`You are already logged in!`);
         }
     } catch (error) {
         console.error("Login error:", error);
-        document.getElementById("status").innerText = "Login failed!";
+        appear(document.getElementById("status"));
+        document.getElementById("status").innerText = error;
     }
 });
+
+let appear = (element) => {
+    if (element.classList.contains("hidden")) {
+        element.classList.remove("hidden");
+    }
+}
+
+let isLogged = async () => {
+    const profile = localStorage.getItem("profile");
+    return !!profile;
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    if (await isLogged()) {
+        document.getElementById("status").innerText = "You are already logged in!";
+
+        try {
+            const response = await fetch("http://localhost:3000/auth/me", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                credentials: "include"
+            });
+            if(response.ok) {
+            alert("You are already logged in!");
+            }
+        } catch (e) {
+            console.error("Error:", e);
+
+
+        }
+    }
+})
